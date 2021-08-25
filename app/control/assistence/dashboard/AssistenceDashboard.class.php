4cf956c3bd5935bd9ac95723beb890f619d070b9
FranklysG <tunele095@gmail.com>
2021-08-25 01:30:12 -0300

    ajustes na dash board de todos os canais

--- app/control/assistence/dashboard/AssistenceDashboard.class.php
+++ app/control/assistence/dashboard/AssistenceDashboard.class.php
@@ -19,14 +19,20 @@ class AssistenceDashboard extends TPage
         
         try
         {
+           
             TTransaction::open('app');
             // mapa_reservas semanais
             $criteria = new TCriteria;
-            $criteria->add(new TFilter('id', '=', TSession::getValue('userid')));
+            // citerios especificos
+            $system_user_unit = SystemUserUnit::where('system_unit_id','=', TSession::getValue('userunitid'))->load();
+            foreach ($system_user_unit as $value) {
+                $ids[] = $value->system_user_id;
+            }
+            $criteria->add(new TFilter('id','IN', $ids));
             $repositoy = new TRepository('ViewFinancial');
             $objects = $repositoy->load($criteria);
 
-            $html = new THtmlRenderer('app/resources/system_assistence_dashboard.html');
+            $html = new THtmlRenderer('app/resources/system_roost_dashboard.html');
             
             TTransaction::open('permission');
             $indicator1 = new THtmlRenderer('app/resources/info-box.html');
@@ -39,18 +45,34 @@ class AssistenceDashboard extends TPage
             $indicator8 = new THtmlRenderer('app/resources/info-box.html');
             $indicator9 = new THtmlRenderer('app/resources/info-box.html');
             $indicator10 = new THtmlRenderer('app/resources/info-box.html');
-            
+
+            $sale_today = 0;
+            $sale_cash_today = 0;
+            $sale_cash_month = 0;
+            $sale_cash_year = 0;
+            $exes_cash_month = 0;
+            $exes_cash_year = 0;
+            $payable_cash_month = 0;
+            $payable_cash_year = 0;
             foreach ($objects as $object) {
-                $indicator1->enableSection('main', ['title' => 'Vendas hoje',    'icon' => 'cart-arrow-down',       'background' => 'orange', 'value' => $object->sale_today]);
-                $indicator2->enableSection('main', ['title' => 'Faturamento Hoje',   'icon' => 'money-bill',      'background' => 'blue',   'value' => Convert::toMonetario($object->sale_cash_today)]);
-                $indicator3->enableSection('main', ['title' => 'Faturamento Mês',   'icon' => 'money-bill-wave',      'background' => 'yellow',   'value' => Convert::toMonetario($object->sale_cash_month)]);
-                $indicator4->enableSection('main', ['title' => 'Faturamento ano',    'icon' => 'wallet', 'background' => 'purple', 'value' => Convert::toMonetario($object->sale_cash_year)]);
-                $indicator5->enableSection('main', ['title' => 'Despesas Mês', 'icon' => 'handshake',       'background' => 'red',  'value' => Convert::toMonetario($object->exes_cash_month)]);
-                $indicator6->enableSection('main', ['title' => 'Despesas Ano', 'icon' => 'handshake',       'background' => 'red',  'value' => Convert::toMonetario($object->exes_cash_year)]);
-                $indicator7->enableSection('main', ['title' => 'Contas a pagar Mês', 'icon' => 'handshake',       'background' => 'red',  'value' => Convert::toMonetario($object->payable_cash_month)]);
-                $indicator8->enableSection('main', ['title' => 'Contas a pagar Ano', 'icon' => 'handshake',       'background' => 'red',  'value' => Convert::toMonetario($object->payable_cash_year)]);
-                $indicator9->enableSection('main', ['title' => 'Lucro esperado Mês', 'icon' => 'wallet',       'background' => 'green',  'value' => Convert::toMonetario(($object->sale_cash_month)-($object->payable_cash_month)-($object->exes_cash_month))]);
-                $indicator10->enableSection('main', ['title' => 'Lucro esperado Ano', 'icon' => 'cash-register',       'background' => 'green',  'value' => Convert::toMonetario(($object->sale_cash_year)-($object->payable_cash_year)-($object->exes_cash_year))]);
+                $sale_today += $object->sale_today;
+                $sale_cash_today += $object->sale_cash_today;
+                $sale_cash_month += $object->sale_cash_month;
+                $sale_cash_year += $object->sale_cash_year;
+                $exes_cash_month += $object->exes_cash_month;
+                $exes_cash_year += $object->exes_cash_year;
+                $payable_cash_month += $object->payable_cash_month;
+                $payable_cash_year += $object->payable_cash_year;
+                $indicator1->enableSection('main', ['title' => 'Vendas hoje',    'icon' => 'cart-arrow-down',       'background' => 'orange', 'value' => $sale_today]);
+                $indicator2->enableSection('main', ['title' => 'Faturamento Hoje',   'icon' => 'money-bill',      'background' => 'blue',   'value' => Convert::toMonetario($sale_cash_today)]);
+                $indicator3->enableSection('main', ['title' => 'Faturamento Mês',   'icon' => 'money-bill-wave',      'background' => 'yellow',   'value' => Convert::toMonetario($sale_cash_month)]);
+                $indicator4->enableSection('main', ['title' => 'Faturamento ano',    'icon' => 'wallet', 'background' => 'purple', 'value' => Convert::toMonetario($sale_cash_year)]);
+                $indicator5->enableSection('main', ['title' => 'Despesas Mês', 'icon' => 'handshake',       'background' => 'red',  'value' => Convert::toMonetario($exes_cash_month)]);
+                $indicator6->enableSection('main', ['title' => 'Despesas Ano', 'icon' => 'handshake',       'background' => 'red',  'value' => Convert::toMonetario($exes_cash_year)]);
+                $indicator7->enableSection('main', ['title' => 'Contas a pagar Mês', 'icon' => 'handshake',       'background' => 'red',  'value' => Convert::toMonetario($payable_cash_month)]);
+                $indicator8->enableSection('main', ['title' => 'Contas a pagar Ano', 'icon' => 'handshake',       'background' => 'red',  'value' => Convert::toMonetario($payable_cash_year)]);
+                $indicator9->enableSection('main', ['title' => 'Lucro esperado Mês', 'icon' => 'wallet',       'background' => 'green',  'value' => Convert::toMonetario(($sale_cash_month)-($payable_cash_month)-($exes_cash_month))]);
+                $indicator10->enableSection('main', ['title' => 'Lucro esperado Ano', 'icon' => 'cash-register',       'background' => 'green',  'value' => Convert::toMonetario(($sale_cash_year)-($payable_cash_year)-($exes_cash_year))]);
             }
             
             $chart = new THtmlRenderer('app/resources/google_column_chart.html');
@@ -59,7 +81,7 @@ class AssistenceDashboard extends TPage
             // média de ocupação mensal
             $meses = AppUtil::calendario();
             $criteria = new TCriteria;
-            $criteria->add(new TFilter('system_user_id', '=', TSession::getValue('userid')));
+            $criteria->add(new TFilter('system_user_id', 'IN', $ids));
             $criteria->add(new TFilter('date(created_at)', 'BETWEEN', date('Y-m-d', strtotime('-1 week')), date('Y-m-d')));
             
             $repositoy = new TRepository('Sale');
--- app/database/views/financial.sql
+++ app/database/views/financial.sql
@@ -1,22 +1,14 @@
 -- CREATE OR REPLACE VIEW `view_financial` AS
-SELECT id
+SELECT syu.id,
+syu.system_unit_id
 ,(SELECT count(id) FROM sale WHERE DATE(sale.created_at) = DATE(NOW()) AND sale.system_user_id = syu.system_unit_id) AS sale_today
-,(SELECT sum(((sale_inventory.price)-(sale_inventory.discount))*sale_inventory.amount) FROM sale_inventory WHERE DATE(sale_inventory.created_at) = DATE(NOW()) AND sale_inventory.system_user_id = syu.system_unit_id) as sale_cash_today
-,(SELECT sum(((sale_inventory.price)-(sale_inventory.discount))*sale_inventory.amount) FROM sale_inventory WHERE DATE(sale_inventory.created_at) BETWEEN DATE_FORMAT(NOW(), "%Y-%m-01") and DATE(NOW()) AND sale_inventory.system_user_id = syu.system_unit_id) AS sale_cash_month
-,(SELECT sum(((sale_inventory.price)-(sale_inventory.discount))*sale_inventory.amount) FROM sale_inventory WHERE DATE(sale_inventory.created_at) BETWEEN DATE_FORMAT(NOW(), "%Y-01-01") and DATE_FORMAT(NOW(), "%Y-12-31") AND sale_inventory.system_user_id = syu.system_unit_id) AS sale_cash_year
-,(SELECT sum(payable.price) FROM payable WHERE DATE(payable.created_at) BETWEEN DATE_FORMAT(NOW(), "%Y-%m-01") and DATE(NOW()) AND payable.status = 0 AND payable.system_user_id = syu.system_unit_id) as payable_cash_month
-,(SELECT sum(payable.price) FROM payable WHERE DATE(payable.created_at) BETWEEN DATE_FORMAT(NOW(), "%Y-01-01") and DATE_FORMAT(NOW(), "%Y-12-31") AND payable.status = 0 AND payable.system_user_id = syu.system_unit_id) as payable_cash_year
-,(SELECT sum(exes.price) FROM exes WHERE DATE(exes.created_at) BETWEEN DATE_FORMAT(NOW(), "%Y-%m-01") and DATE(NOW()) AND exes.system_user_id = syu.system_unit_id) as exes_cash_month 
-,(SELECT sum(exes.price) FROM exes WHERE DATE(exes.created_at) BETWEEN DATE_FORMAT(NOW(), "%Y-01-01") and DATE_FORMAT(NOW(), "%Y-12-31") AND exes.system_user_id = syu.system_unit_id) as exes_cash_year
-,(SELECT sum(employee.salary) FROM employee WHERE DATE(employee.created_at) BETWEEN DATE_FORMAT(NOW(), "%Y-%m-01") and DATE(NOW()) AND employee.system_user_id = syu.system_unit_id) as employee_salary
--- ,(SELECT sum(sd.valor_saida) FROM saida AS sd WHERE DATE(sd.created_at) BETWEEN date_add(DATE(NOW()),interval -1 week) and DATE(NOW())) AS est_saida_semanal, 
--- ,(SELECT sum(sd.valor_saida) FROM saida AS sd WHERE DATE(sd.created_at) BETWEEN DATE_FORMAT(NOW(), "%Y-%m-01") and DATE(NOW())) AS est_saida_mensal,
--- ,(SELECT sum(sd.valor_saida) FROM saida AS sd WHERE DATE(sd.created_at) BETWEEN DATE_FORMAT(NOW(), "%Y-01-01") and DATE(NOW())) AS est_saida_anual,
--- ,(SELECT sum(en.qtd_nota * en.valor_venda_uni) FROM entrada AS en WHERE DATE(en.created_at) BETWEEN date_add(DATE(NOW()),interval -1 week) and DATE(NOW())) AS est_entrada_semanal,
--- ,(SELECT sum(en.qtd_nota * en.valor_venda_uni) FROM entrada AS en WHERE DATE(en.created_at) BETWEEN DATE_FORMAT(NOW(), "%Y-%m-01") and DATE(NOW())) AS est_entrada_mensal,
--- ,(SELECT sum(en.qtd_nota * en.valor_venda_uni) FROM entrada AS en WHERE DATE(en.created_at) BETWEEN DATE_FORMAT(NOW(), "%Y-01-01") and DATE(NOW())) AS est_entrada_anual,
--- ,(SELECT sum(ven.qtd_venda * ven.valor_venda) FROM venda AS ven WHERE DATE(ven.created_at) BETWEEN date_add(DATE(NOW()),interval -1 week) and DATE(NOW())) AS venda_externa_semanal,
--- ,(SELECT sum(ven.qtd_venda * ven.valor_venda) FROM venda AS ven WHERE DATE(ven.created_at) BETWEEN DATE_FORMAT(NOW(), "%Y-%m-01") and DATE(NOW())) AS venda_externa_mensal,
--- ,(SELECT sum(ven.qtd_venda * ven.valor_venda) FROM venda AS ven WHERE DATE(ven.created_at) BETWEEN DATE_FORMAT(NOW(), "%Y-01-01") and DATE(NOW())) AS venda_externa_anual
- FROM system_user as syu
- -- Where syu.system_unit_id = 3
+,(SELECT sum(((sale_inventory.price)-(sale_inventory.discount))*sale_inventory.amount) FROM sale_inventory WHERE DATE(sale_inventory.created_at) = DATE(NOW()) AND sale_inventory.system_user_id = syu.id) as sale_cash_today
+,(SELECT sum(((sale_inventory.price)-(sale_inventory.discount))*sale_inventory.amount) FROM sale_inventory WHERE DATE(sale_inventory.created_at) BETWEEN DATE_FORMAT(NOW(), "%Y-%m-01") and DATE(NOW()) AND sale_inventory.system_user_id = syu.id) AS sale_cash_month
+,(SELECT sum(((sale_inventory.price)-(sale_inventory.discount))*sale_inventory.amount) FROM sale_inventory WHERE DATE(sale_inventory.created_at) BETWEEN DATE_FORMAT(NOW(), "%Y-01-01") and DATE_FORMAT(NOW(), "%Y-12-31") AND sale_inventory.system_user_id = syu.id) AS sale_cash_year
+,(SELECT sum(payable.price) FROM payable WHERE DATE(payable.created_at) BETWEEN DATE_FORMAT(NOW(), "%Y-%m-01") and DATE(NOW()) AND payable.status = 0 AND payable.system_user_id = syu.id) as payable_cash_month
+,(SELECT sum(payable.price) FROM payable WHERE DATE(payable.created_at) BETWEEN DATE_FORMAT(NOW(), "%Y-01-01") and DATE_FORMAT(NOW(), "%Y-12-31") AND payable.status = 0 AND payable.system_user_id = syu.id) as payable_cash_year
+,(SELECT sum(exes.price) FROM exes WHERE DATE(exes.created_at) BETWEEN DATE_FORMAT(NOW(), "%Y-%m-01") and DATE(NOW()) AND exes.system_user_id = syu.id) as exes_cash_month 
+,(SELECT sum(exes.price) FROM exes WHERE DATE(exes.created_at) BETWEEN DATE_FORMAT(NOW(), "%Y-01-01") and DATE_FORMAT(NOW(), "%Y-12-31") AND exes.system_user_id = syu.id) as exes_cash_year
+,(SELECT sum(employee.salary) FROM employee WHERE DATE(employee.created_at) BETWEEN DATE_FORMAT(NOW(), "%Y-%m-01") and DATE(NOW()) AND employee.system_user_id = syu.id) as employee_salary
+FROM system_user as syu
+ -- Where syu.id = 3
